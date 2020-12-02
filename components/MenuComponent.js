8482a4dfac569f8bd17cd6eb1f6c598896f46780
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2020-12-02 17:21:48 +0530

    Native Components I

---
+++ components/MenuComponent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { View, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+function Menu(props){
+
+    const renderMenuItem = ({item, index}) =>{
+        return(
+            <ListItem
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron={true}
+                leftAvatar={{ source: require('./images/alberto.png') }}
+                />
+        );
+    }
+
+    return(
+        <FlatList 
+            data={props.dishes}
+            renderItem={renderMenuItem}
+            keyExtractor={item => item.id.toString()}
+            />
+    );
+}
+
+
+export default Menu;
